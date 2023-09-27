29a7530edf52cbbe76c8ab42ae603c1baf394b5e
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2023-09-27 14:13:32 +0100

    Add solution to median.py

--- median.py
+++ median.py
@@ -1,6 +1,18 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(lst):
+    sorted_lst = sorted(lst)
+    n = len(sorted_lst)
+
+    if n % 2 == 1:
+        return f"The median is: {sorted_lst[n//2]}"
+    else:
+        middle_one = sorted_lst[n // 2 - 1]
+        middle_two = sorted_lst[n // 2]
+        find_median = (middle_one + middle_two) / 2
+        return f"The median is: {find_median}"
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
